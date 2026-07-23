4d5e2e55a728d7c0cad5dda4a6fc978dd9d4474f
keshavsoft <nknnkumar@yahoo.com>
2026-07-23 22:39:05 +0530

    v

--- test/v2/test.js
+++ test/v2/test.js
@@ -1,26 +1,18 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+
 import index from "../../index.js";
+
 import getLatestVersion from "../../bin/core/getLatestVersion.js";
 import checkLines from "./checkLines.json" with { type: "json" };
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const appJsPath = path.join(__dirname, "app.js");
-``
-const initialAppContent = fs.readFileSync(appJsPath, "utf8");
-
-function resetAppJs() {
-    fs.writeFileSync(appJsPath, initialAppContent, "utf8");
-}
 
 function runTests() {
     const latestVersion = getLatestVersion();
-    console.log(`Starting ${latestVersion} tests...`);
 
-    // Test Case 1: Simple template from checkLines.json
-    resetAppJs();
-    console.log("\n--- Test Case 1: Simple template ---");
     const checkLinesString = checkLines.simple;
 
     const output = index({
@@ -29,24 +21,7 @@ function runTests() {
         inCheckLines: checkLinesString
     });
 
-    let result = fs.readFileSync(appJsPath, "utf8");
-    if (
-        output &&
-        output.importResult &&
-        output.importResult.found === false &&
-        output.useResult &&
-        output.useResult.found === false &&
-        result.includes("import funcFrom${folderName} from './${folderName}/controller.js';") &&
-        result.includes("router.get('/${endpoint}'")
-    ) {
-        console.log("✅ Test Case 1 Passed: Simple template inserted correctly and output is correct.");
-    } else {
-        console.error("❌ Test Case 1 Failed: Simple template insertion failed or output incorrect.");
-        console.error("Returned output was:", output);
-        process.exit(1);
-    }
-
-    console.log("\nAll tests completed successfully!");
-}
+    console.log("output : ", output);
+};
 
 runTests();
