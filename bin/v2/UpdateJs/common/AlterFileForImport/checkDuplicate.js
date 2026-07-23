c53c239309762b4e86d68f57a2014c1465c158cc
keshavsoft <nknnkumar@yahoo.com>
2026-07-23 22:46:58 +0530

    v

--- bin/v2/UpdateJs/common/AlterFileForImport/checkDuplicate.js
+++ bin/v2/UpdateJs/common/AlterFileForImport/checkDuplicate.js
@@ -1,8 +1,19 @@
 import { isImportPresent } from "express-check-any-for-import";
+import fromPatternCollector from "pattern-collector-anyjs";
+
+import extractRegex from './extractRegex.js';
 
 const checkUseDuplicate = ({ inContent, inFilePath, inSearchText }) => {
     const cleanText = inSearchText.match(/['"]([^'"]+)['"]/)?.[1] || inSearchText;
     const found = isImportPresent(inContent, cleanText) || inContent.includes(inSearchText);
+
+    const k1 = fromPatternCollector({
+        fileContent: inContent,
+        extractRegex
+    });
+
+    console.log("aaaaaaaaa : ", k1);
+
 
     return {
         found,
